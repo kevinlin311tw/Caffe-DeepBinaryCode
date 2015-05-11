25ea599fcc0a6b60b174b5e2c116c9200f64fcc8
kevinlin311tw <kevinlin311.tw@gmail.com>
2015-05-11 14:32:24 +0800

    new model with new objective functions

--- src/caffe/layer_factory.cpp
+++ src/caffe/layer_factory.cpp
@@ -251,6 +251,10 @@ Layer<Dtype>* GetLayer(const LayerParameter& param) {
     return GetTanHLayer<Dtype>(name, param);
   case LayerParameter_LayerType_WINDOW_DATA:
     return new WindowDataLayer<Dtype>(param);
+  case LayerParameter_LayerType_K1_EUCLIDEAN_LOSS:
+    return new K1_EuclideanLossLayer<Dtype>(param);
+  case LayerParameter_LayerType_K2_EUCLIDEAN_LOSS:
+    return new K2_EuclideanLossLayer<Dtype>(param);
   case LayerParameter_LayerType_NONE:
     LOG(FATAL) << "Layer " << name << " has unspecified type.";
   default:
